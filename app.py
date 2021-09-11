7d61418efd05a7527c3c48d41b5e08f3809cfafa
PKM25101970 <78253997+PKM25101970@users.noreply.github.com>
2021-09-11 20:25:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+import joblib
+app= Flask(__name__)
+model = joblib.load('dia_79.pkl')
+
+@app.route('/')
+def hello_word():
+    return render_template('home.html')
+
+@app.route('/predict', methods = ["POST"])
+def predict():
+    preg= request.form.get('preg')
+    plas= request.form.get('plas')
+    pres = request.form.get('pres')
+    skin = request.form.get('skin')
+    test = request.form.get('test')
+    mass= request.form.get('mass')
+    pedi = request.form.get('pedi')
+    age = request.form.get('age')
+
+    print(preg,plas,pres , skin , test, mass, pedi, age)
+
+    output = model.predict([[preg,plas,pres,skin,test, mass, pedi, age]])
+    if output[0] ==1:
+        ans="Diabatic"
+    else:
+        ans="Non Diabetic"
+
+    return render_template('predict.html',predict= f'The person is {ans}')    
+    
+    
+    return 'Form Submited'
+
+# @app.route('/Welcome')
+# def welcome():
+#     return 'Welecone to you'
+
+# @app.route('/contact')
+# def contact():
+#     return render_template('contact.html')
+
+# @app.route('/Gallary2')
+# def Gallary():
+#     return'Welecone to Gallery'
+
+app.run(debug=True)
